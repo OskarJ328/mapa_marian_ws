8e718f7088865574d9e0e09eb007bc5faf10a01f
OskarJ328 <250966@edu.p.lodz.pl>
2025-07-30 15:44:49 +0200

    stworzylem repo

---
+++ src/my_tf_broadcaster/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_tf_broadcaster'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='r4h',
+    maintainer_email='r4h@todo.todo',
+    description='Static TF broadcaster for ROS 2',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'odom_to_tf = my_tf_broadcaster.odom_to_tf:main',
+        ],
+    },
+)
